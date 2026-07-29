a992de6c0b6f71c092f0295d3171e27b30af77f8
websitehelpdesk2-ui <websitehelpdesk2@gmail.com>
2026-07-29 07:14:43 +0000

    Force preview URLs on every request

--- public_html/wp-content/themes/am-training-theme/functions.php
+++ public_html/wp-content/themes/am-training-theme/functions.php
@@ -1,11 +1,17 @@
 <?php
-update_option('siteurl', 'https://musical-space-spoon-q7w9jgq4v97q3xrpg-8080.app.github.dev');
-update_option('home', 'https://musical-space-spoon-q7w9jgq4v97q3xrpg-8080.app.github.dev');
+function am_training_force_preview_urls() {
+    $preview_url = 'https://musical-space-spoon-q7w9jgq4v97q3xrpg-8080.app.github.dev';
 
-add_filter('site_url', function($url) {
+    update_option('siteurl', $preview_url);
+    update_option('home', $preview_url);
+}
+
+add_action('init', 'am_training_force_preview_urls');
+
+add_filter('site_url', function ($url) {
     return str_replace('localhost', 'musical-space-spoon-q7w9jgq4v97q3xrpg-8080.app.github.dev', $url);
 }, 10, 3);
 
-add_filter('home_url', function($url) {
+add_filter('home_url', function ($url) {
     return str_replace('localhost', 'musical-space-spoon-q7w9jgq4v97q3xrpg-8080.app.github.dev', $url);
 }, 10, 3);
