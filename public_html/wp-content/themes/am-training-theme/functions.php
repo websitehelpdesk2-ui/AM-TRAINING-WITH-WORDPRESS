a38949110b8f022439c6e35728afc80f74bb2ba5
websitehelpdesk2-ui <websitehelpdesk2@gmail.com>
2026-07-29 07:13:01 +0000

    Force Codespaces URL in theme functions

--- public_html/wp-content/themes/am-training-theme/functions.php
+++ public_html/wp-content/themes/am-training-theme/functions.php
@@ -1,4 +1,7 @@
 <?php
+update_option('siteurl', 'https://musical-space-spoon-q7w9jgq4v97q3xrpg-8080.app.github.dev');
+update_option('home', 'https://musical-space-spoon-q7w9jgq4v97q3xrpg-8080.app.github.dev');
+
 add_filter('site_url', function($url) {
     return str_replace('localhost', 'musical-space-spoon-q7w9jgq4v97q3xrpg-8080.app.github.dev', $url);
 }, 10, 3);
